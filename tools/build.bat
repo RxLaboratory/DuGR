7c67f65c0a2230c4191927c281aaaa94e147a106
Duduf <git@duduf.com>
2021-08-25 12:14:04 +0200

    Update build.bat

--- tools/build.bat
+++ tools/build.bat
@@ -4,14 +4,15 @@ rmdir /s /q docs
 mkdir docs
 cd tools
 mkdir output
-DuBuilder ..\Dugr.jsx output\DuGR.jsxinc
-DuBuilder ..\inc\api.jsxinc output\DuGR_api.jsxinc
-DuBuilder ..\inc\api_all.jsxinc -d jsdoc_conf.json output\DuAEF_DuGR_api.jsxinc
+DuBuilder ..\Dugr.jsx output\DuGR.jsx -nobanner
+DuBuilder ..\inc\api.jsxinc output\DuGR_api.jsxinc -nobanner
+DuBuilder ..\inc\api_all.jsxinc -nobanner -d jsdoc_conf.json output\DuAEF_DuGR_api.jsxinc
 cd output
 xcopy /Y api.jsxinc Dugr_api.jsxinc
 cd ..
 xcopy /S /I /Y ..\docs output\docs
 xcopy /S /I /Y ..\DuGR_Docs\docs output\Help
+rm -f output\Help\CNAME
 cd output\docs
 xcopy /Y DuGR.html index.html
 cd ..
