b70f764b82bc92f34f1ad44f333076e95674ea02
Duduf <git@duduf.com>
2022-07-18 18:55:47 +0200

    4.0.4 release

--- tools/build/build.bat
+++ tools/build/build.bat
@@ -1,48 +1,61 @@
-cd ..
-cd ..
-del /s /q docs
-rmdir /s /q docs
-mkdir docs
-cd DuGR_Docs
-cd src
+@echo off
+
+:: The repo (current dir)
+SET repoPath=%~dp0..\..
+
+:: The build path
+SET build_path=%~dp0build
+
+echo Building "%repoPath%" in "%build_path%"...
+
+:: Clean
+rd /s /q "%build_path%"
+md "%build_path%"
+
+:: Build folders
+md "%build_path%\DuGR"
+md "%build_path%\DuGR\ScriptUI Panels"
+md "%build_path%\DuGR\Help"
+md "%build_path%\DuGR_API"
+
+:: Build API
+DuBuilder "%repoPath%\inc\api.jsxinc" --no-banner "%build_path%\DuGR_API\DuGR_api.jsxinc"
+DuBuilder "%repoPath%\inc\api_all.jsxinc" --no-banner "%build_path%\DuGR_API\DuAEF_DuGR_api.jsxinc"
+
+:: Build DuGR
+DuBuilder "%repoPath%\DuGR.jsx" --no-banner "%build_path%\DuGR\ScriptUI Panels\DuGR.jsx"
+
+:: Build API reference
+:: clean first
+rd /s /q "%repoPath%\docs"
+md "%repoPath%\docs"
+cmd /c jsdoc -c jsdoc_conf.json
+echo " " > "%repoPath%\docs\jsdoc.css"
+xcopy "%repoPath%\tools\build\jsdoc.css" "%repoPath%\docs\jsdoc.css"
+xcopy "%repoPath%\docs\DuGR.html" "%repoPath%\docs\index.html"
+
+:: Build Guide
+cd "%repoPath%\DuGR_Docs\src"
 mkdocs build
-cd ..
-cd ..
-cd tools
-cd build
-mkdir output
-mkdir "output\DuGR"
-mkdir "output\DuGR\ScriptUI Panels"
-mkdir "output\DuGR\Help"
-mkdir "output\DuGR_API"
-DuBuilder ..\..\inc\api.jsxinc --no-banner output\DuGR_API\DuGR_api.jsxinc
-DuBuilder ..\..\inc\api_all.jsxinc --no-banner -d jsdoc_conf.json output\DuGR_API\DuAEF_DuGR_api.jsxinc
-DuBuilder ..\..\Dugr.jsx --no-banner "output\DuGR\ScriptUI Panels\DuGR.jsx"
-echo " " > "output\DuGR\LICENSE.md"
-echo " " > "output\DuGR\LICENSE.txt"
-echo " " > "output\DuGR\README.txt"
-echo " " > "output\DuGR_API\LICENSE.md"
-echo " " > "output\DuGR_API\LICENSE.txt"
-xcopy /Y items\LICENSE.md "output\DuGR\LICENSE.md"
-xcopy /Y items\LICENSE.txt "output\DuGR\LICENSE.txt"
-xcopy /Y items\README.txt "output\DuGR\README.txt"
-xcopy /Y items\LICENSE.md "output\DuGR_API\LICENSE.md"
-xcopy /Y items\LICENSE.txt "output\DuGR_API\LICENSE.txt"
-xcopy /S /I /Y ..\..\docs output\DuGR_API\docs
-xcopy /S /I /Y ..\..\DuGR_Docs\docs output\DuGR\Help
-cd output\DuGR\Help
-del CNAME
-cd ..
-cd ..
-cd ..
-cd output\DuGR_API\docs
-xcopy /Y DuGR.html index.html
-cd ..
-cd ..
-cd ..
-cd ..
-cd ..
-cd docs
-xcopy /Y DuGR.html index.html
-echo dugr.rxlab.io > "CNAME"
+cd "%repoPath%\tools\build"
+
+:: Copy items
+echo " " > "%build_path%\DuGR\LICENSE.md"
+echo " " > "%build_path%\DuGR\LICENSE.txt"
+echo " " > "%build_path%\DuGR\README.txt"
+echo " " > "%build_path%\DuGR_API\LICENSE.md"
+echo " " > "%build_path%\DuGR_API\LICENSE.txt"
+xcopy /Y "%repoPath%\tools\build\items\LICENSE.md" "%build_path%\DuGR\LICENSE.md"
+xcopy /Y "%repoPath%\tools\build\items\LICENSE.txt" "%build_path%\DuGR\LICENSE.txt"
+xcopy /Y "%repoPath%\tools\build\items\README.txt" "%build_path%\DuGR\README.txt"
+xcopy /Y "%repoPath%\tools\build\items\LICENSE.md" "%build_path%\DuGR_API\LICENSE.md"
+xcopy /Y "%repoPath%\tools\build\items\LICENSE.txt" "%build_path%\DuGR_API\LICENSE.txt"
+
+:: Copy the API doc
+xcopy /S /I /Y "%repoPath%\docs" "%build_path%\DuGR_API\docs"
+
+:: Copy the guide
+xcopy /S /I /Y "%repoPath%\DuGR_Docs\docs" "%build_path%\DuGR\Help"
+
+echo Done !
 pause
