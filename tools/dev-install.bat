c4a179a7e45ca29e5d2f21568000275b3d6601ec
Duduf <git@duduf.com>
2022-10-29 14:00:21 +0200

    performance improvements

--- tools/dev-install.bat
+++ tools/dev-install.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: The path to ScriptUI Panels
-SET "scriptui_panels=C:\Program Files\Adobe\Adobe After Effects 2022\Support Files\Scripts\ScriptUI Panels"
+SET "scriptui_panels=C:\Program Files\Adobe\Adobe After Effects 2023\Support Files\Scripts\ScriptUI Panels"
 
 :: The repo (current dir)
 SET repoPath=%~dp0..
