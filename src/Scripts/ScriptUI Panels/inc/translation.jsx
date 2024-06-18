099e1b5ad5deff26aa8924511de2ab1b63b00088
Duduf <git@duduf.com>
2024-06-18 13:49:41 +0200

    Update translations

--- src/Scripts/ScriptUI Panels/inc/translation.jsx
+++ src/Scripts/ScriptUI Panels/inc/translation.jsx
@@ -5,12 +5,13 @@ DuESF.preInitMethods.push(function ()
     // Extract translations
     var outputFolder = DuESF.scriptSettings.file.parent.absoluteURI + '/';
 
-    DuGR_de.toFile( outputFolder + 'DuGR_de.json', false );
-    DuGR_eo.toFile( outputFolder + 'DuGR_eo.json', false );
-    DuGR_es.toFile( outputFolder + 'DuGR_es.json', false );
-    DuGR_fr.toFile( outputFolder + 'DuGR_fr.json', false );
-    DuGR_ja.toFile( outputFolder + 'DuGR_ja.json', false );
-    DuGR_pd.toFile( outputFolder + 'DuGR_pd.json', false );
-    DuGR_ru.toFile( outputFolder + 'DuGR_ru.json', false );
-    DuGR_zh.toFile( outputFolder + 'DuGR_zh.json', false );
+    DuGR_de_DE.toFile( outputFolder + 'DuGR_de.json', false );
+    DuGR_eo_UY.toFile( outputFolder + 'DuGR_eo.json', false );
+    DuGR_es_ES.toFile( outputFolder + 'DuGR_es.json', false );
+    DuGR_fr_FR.toFile( outputFolder + 'DuGR_fr.json', false );
+    //DuGR_ja.toFile( outputFolder + 'DuGR_ja.json', false );
+    DuGR_pd_FR.toFile( outputFolder + 'DuGR_pd.json', false );
+    DuGR_ru_RU.toFile( outputFolder + 'DuGR_ru.json', false );
+    DuGR_zh_CN.toFile( outputFolder + 'DuGR_zh.json', false );
+    DuGR_zh_TW.toFile( outputFolder + 'DuGR_zh_TW.json', false );
 } );
